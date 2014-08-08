06acea77b061a7f665e2590aba9832f2b9963e76
kokonech <okonechnikov@mpiib-berlin.mpg.de>
2014-08-08 16:42:35 +0200

    Add MultiBamQC sample

---
+++ qualimap/generate_samples.sh
@@ -0,0 +1,10 @@
+
+QUALIMAP_CMD=qualimap
+
+
+# Multisample BAM QC
+
+$QUALIMAP_CMD multi-bamqc -i /data/qualimap_release_data/multisample_bamqc/gh2ax_chip_seq.txt -outdir /home/kokonech/playgrnd/kokonech.github.io/qualimap/gh2ax_multibamqc
+
+
+
