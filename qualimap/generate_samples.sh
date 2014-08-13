2aa46abcbf1249e0875d044c41ba4c2748f2a1e3
kokonech <okonechnikov@mpiib-berlin.mpg.de>
2014-08-13 16:03:03 +0200

    Add Counts QC reports and sample data

--- qualimap/generate_samples.sh
+++ qualimap/generate_samples.sh
@@ -2,9 +2,25 @@
 QUALIMAP_CMD=qualimap
 
 
+# BAM QC
+
+
+
+# BAM QC with regions
+
+
+
+# RNA-seq QC
+
+
+
 # Multisample BAM QC
 
 $QUALIMAP_CMD multi-bamqc -i /data/qualimap_release_data/multisample_bamqc/gh2ax_chip_seq.txt -outdir /home/kokonech/playgrnd/kokonech.github.io/qualimap/gh2ax_multibamqc
+
+# Counts QC
+
+$QUALIMAP_CMD counts -d /data/qualimap_release_data/counts/GlcN_mice/countsqc_input.txt -c -s mouse -outdir /home/kokonech/playgrnd/kokonech.github.io/qualimap/glcn_mice_counts
 
 
 
