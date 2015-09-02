d9d3a2f4bfcde2db2cc7875de0495a7378eec1cf
kokonech <okonechnikov@mpiib-berlin.mpg.de>
2015-09-02 09:40:52 +0200

    Update BAM QC reports

--- qualimap/generate_samples.sh
+++ qualimap/generate_samples.sh
@@ -4,9 +4,9 @@ QUALIMAP_CMD=qualimap
 
 # BAM QC
 
-$QUALIMAP_CMD bamqc -bam /data/qualimap_release_data/alignments/HG00096.chrom20.bam -c -gd human -outdir /home/kokonech/playgrnd/kokonech.github.io/qualimap/HG00096.chr20_bamqc
+#$QUALIMAP_CMD bamqc -bam /data/qualimap_release_data/alignments/HG00096.chrom20.bam -c -gd human -outdir HG00096.chr20_bamqc
 
-$QUALIMAP_CMD bamqc -bam /data/qualimap_release_data/alignments/ERR089819.bam -c -outfile /home/kokonech/playgrnd/kokonech.github.io/qualimap/ERR089819_report.pdf
+$QUALIMAP_CMD bamqc -bam /data/qualimap_release_data/alignments/ERR089819.bam -c -outfile ERR089819_report.pdf -outdir .
 
 # BAM QC with regions
 
@@ -14,18 +14,18 @@ $QUALIMAP_CMD bamqc -bam /data/qualimap_release_data/alignments/ERR089819.bam -c
 
 # RNA-seq QC
 
-$QUALIMAP_CMD rnaseq -bam /data/qualimap_release_data/counts/kidney.bam -gtf /data/qualimap_release_data/annotations/human.64.gtf -oc /home/kokonech/tmp/kidney.counts -outdir /home/kokonech/playgrnd/kokonech.github.io/qualimap/kidney_rnaseqqc
+#$QUALIMAP_CMD rnaseq -bam /data/qualimap_release_data/counts/kidney.bam -gtf /data/qualimap_release_data/annotations/human.64.gtf -oc /home/kokonech/tmp/kidney.counts -outdir /home/kokonech/playgrnd/kokonech.github.io/qualimap/kidney_rnaseqqc
 
 
 # Multisample BAM QC
 
-$QUALIMAP_CMD multi-bamqc -d /data/qualimap_release_data/multisample_bamqc/gh2ax_chip_seq.txt -outdir /home/kokonech/playgrnd/kokonech.github.io/qualimap/gh2ax_multibamqc
+#$QUALIMAP_CMD multi-bamqc -d /data/qualimap_release_data/multisample_bamqc/gh2ax_chip_seq.txt -outdir /home/kokonech/playgrnd/kokonech.github.io/qualimap/gh2ax_multibamqc
 
 # Counts QC
 
-$QUALIMAP_CMD counts -d /data/qualimap_release_data/counts/GlcN_mice/countsqc_input.txt -c -s mouse -outdir /home/kokonech/playgrnd/kokonech.github.io/qualimap/glcn_mice_counts
+#$QUALIMAP_CMD counts -d /data/qualimap_release_data/counts/GlcN_mice/countsqc_input.txt -c -s mouse -outdir /home/kokonech/playgrnd/kokonech.github.io/qualimap/glcn_mice_counts
 
-$QUALIMAP_CMD counts -d /home/kokonech/playgrnd/kokonech.github.io/qualimap/samples/marioni_countsqc_input.txt -c -s human -outdir /home/kokonech/playgrnd/kokonech.github.io/qualimap/marioni_counts
+#$QUALIMAP_CMD counts -d /home/kokonech/playgrnd/kokonech.github.io/qualimap/samples/marioni_countsqc_input.txt -c -s human -outdir /home/kokonech/playgrnd/kokonech.github.io/qualimap/marioni_counts
 
 
 
